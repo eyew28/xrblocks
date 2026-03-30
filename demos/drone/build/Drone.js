cfec77165f94af0454a0e258f54c307c3a7ef5eb
eyew-xrg <eyew@redcartel.com.au>
2026-03-30 16:48:29 +1100

    updated build drone

--- demos/drone/build/Drone.js
+++ demos/drone/build/Drone.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
 import * as xb from 'xrblocks';
 import { DroneKeyboardControls } from './DroneKeyboardControls.js';
-const DRONE_FBX_FILE = 'https://cdn.jsdelivr.net/gh/xrblocks/assets@main/demos/drone/DroneModel/Drone.fbx';
+const DRONE_FBX_FILE = '../models/ace-pilot.fbx';
 export class Drone extends xb.Script {
     moveSpeed;
     rotationSpeed;
@@ -67,13 +67,14 @@ export class Drone extends xb.Script {
         const rotation = new THREE.Quaternion().copy(rigidBody.rotation());
         const leftControllerAxes = xb.core.input.leftController?.gamepad?.axes;
         const rightControllerAxes = xb.core.input.rightController?.gamepad?.axes;
+        // Mode 2: left stick = yaw + throttle, right stick = horizontal strafe.
         if (leftControllerAxes && leftControllerAxes.length >= 4) {
-            controllerForce.x += this.moveSpeed * leftControllerAxes[2];
-            controllerForce.z += this.moveSpeed * leftControllerAxes[3];
+            rotation.premultiply(new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), -this.rotationSpeed * leftControllerAxes[2]));
+            controllerForce.y -= this.moveSpeed * leftControllerAxes[3];
         }
         if (rightControllerAxes && rightControllerAxes.length >= 4) {
-            rotation.premultiply(new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), -this.rotationSpeed * rightControllerAxes[2]));
-            controllerForce.y -= this.moveSpeed * rightControllerAxes[3];
+            controllerForce.x += this.moveSpeed * rightControllerAxes[2];
+            controllerForce.z += this.moveSpeed * rightControllerAxes[3];
         }
         // Add keyboard controls.
         // Clamp the controller force to moveSpeed.
